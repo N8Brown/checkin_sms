6023c43dc9826fc62bdf8a160664f863d70562a0
Nate Brown <nathan.a.brown@outlook.com>
2021-08-15 17:36:14 -0400

    Add login, register, and dashboard templates and views

--- text/views.py
+++ text/views.py
@@ -1,4 +1,7 @@
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.http import HttpResponse, HttpResponseForbidden
 from django.shortcuts import redirect, render
@@ -13,7 +16,50 @@ import os
 
 
 def home(request):
-    return render(request, 'text/checkinsms.html', {})
+    context = {}
+    return render(request, 'text/checkinsms.html', context)
+
+
+def user_register(request):
+    context = {}
+    return render(request, 'text/register.html', context)
+
+
+def user_login(request):
+    
+    if request.method == "POST":
+        username = request.POST['login_username']
+        password = request.POST['login_password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('user_dashboard', user.username.lower())
+        else:
+            messages.warning(request, ('Username or password is incorrect. Please try again.'))
+            return redirect('user_login')
+
+    else:
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            context = {}            
+            return render(request, 'text/login.html', context)
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
+
+
+@login_required
+def user_dashboard(request, username):
+    current_user = request.user
+    if current_user.username == username:
+        context = {}
+        return render(request, 'text/dashboard.html', context)
+    else:
+        return redirect('user_login')
 
 
 def client_form(request):
@@ -32,10 +78,10 @@ def client_form(request):
             context = {
                 'form':form,
             }
-            return render(request, 'text/client_form.html', context)
+            return render(request.lower(), 'text/client_form.html', context)
     else:
         context = {}
-        return render(request, 'text/client_form.html', context)
+        return render(request.lower(), 'text/client_form.html', context)
     
 
 
@@ -70,35 +116,34 @@ def validate_twilio_request(func):
 
 
 
-def checkin(request):
-    if request.method == 'POST':
-        print(request.POST)
-        form = PhoneForm(request.POST or None)
-        if form.is_valid():
-            form.save()
-            # Use success message until Confirmation template is created, then redirect to Confirmation page.
-            messages.success(request, ('Your phone number has been successfully added to the list!'))
+# def checkin(request):
+#     if request.method == 'POST':
+#         print(request.POST)
+#         form = PhoneForm(request.POST or None)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, ('Your phone number has been successfully added to the list!'))
 
-            phone_list = Phone.objects.filter(is_active=True)
-            context = {
-                'phone_list':phone_list,
-            }
+#             phone_list = Phone.objects.filter(is_active=True)
+#             context = {
+#                 'phone_list':phone_list,
+#             }
 
-            return redirect(checkin)
-        else:
-            phone_list = Phone.objects.filter(is_active=True)
-            context = {
-                'form':form,
-                'phone_list':phone_list,
-            }
-            return render(request, 'text/checkin.html', context)
+#             return redirect(checkin)
+#         else:
+#             phone_list = Phone.objects.filter(is_active=True)
+#             context = {
+#                 'form':form,
+#                 'phone_list':phone_list,
+#             }
+#             return render(request, 'text/checkin.html', context)
 
-    else:
-        phone_list = Phone.objects.filter(is_active=True)
-        context = {
-            'phone_list':phone_list,
-        }
-        return render(request, 'text/checkin.html', context)
+#     else:
+#         phone_list = Phone.objects.filter(is_active=True)
+#         context = {
+#             'phone_list':phone_list,
+#         }
+#         return render(request, 'text/checkin.html', context)
 
 
 
